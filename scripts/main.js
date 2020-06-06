481530a82c8236ae70d088084c5861e76c8d6eae
radartrinkets <navdmarel@gmail.com>
2020-06-06 15:35:05 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var boxEl = document.querySelector('a-box');
+var boxEl = document.querySelector('a-sphere');
   boxEl.addEventListener('mouseenter', function () {
     boxEl.setAttribute('scale', {x: 2, y: 2, z: 2});
   });
